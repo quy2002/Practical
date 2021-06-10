e9e23ed4069b0b53deb3caf8a5fcfe090318e1fd
quy2002 <71065274+quy2002@users.noreply.github.com>
2021-06-10 10:55:19 +0700

    Update Phone.cs

--- source/repos/C-Sharp/C-Sharp/Practical/Excercise_1/Phone.cs
+++ source/repos/C-Sharp/C-Sharp/Practical/Excercise_1/Phone.cs
@@ -1,12 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 
-namespace C_Sharp.Practical.Excercise_1
+namespace T2009M.Practical
 {
-    class Phone
+    public class Phone
     {
+        protected string phoneName;
+        protected string phoneType;
+        protected float phonePrice;
+
+        public Phone()
+        {
+            
+        }
+
+        public Phone(string phoneName, string phoneType, float phonePrice)
+        {
+            this.phoneName = phoneName;
+            this.phoneType = phoneType;
+            this.phonePrice = phonePrice;
+        }
+        public string PhoneName
+        {
+            get => phoneName;
+            set => phoneName = value;
+        }
+
+        public string PhoneType
+        {
+            get => phoneType;
+            set => phoneType = value;
+        }
+
+        public float PhonePrice
+        {
+            get => phonePrice;
+            set => phonePrice = value;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("Phone: " + PhoneName);
+            Console.WriteLine("Type: " + PhoneType);
+            Console.WriteLine("Price: " + PhonePrice);
+        }
     }
 }
