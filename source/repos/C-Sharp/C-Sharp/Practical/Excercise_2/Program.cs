4ea3d563e45ab81361f62984c35cd4a164a89d25
quy2002 <71065274+quy2002@users.noreply.github.com>
2021-06-10 10:56:06 +0700

    Update Program.cs

--- source/repos/C-Sharp/C-Sharp/Practical/Excercise_2/Program.cs
+++ source/repos/C-Sharp/C-Sharp/Practical/Excercise_2/Program.cs
@@ -1,12 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
 
-namespace C_Sharp.Practical.Excercise_2
+namespace T2009M.Practical.Excercise_2
 {
-    class Program
+    public delegate void PrimeNumberFinder(int primeNumber);
+    public class Program
     {
+        public static event PrimeNumberFinder OnPrimeNumber;
+        
+        public static PrimeNumberFinder pnf = new PrimeNumberFinder(NoticePrimeNumber);
+        
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Số nguyên tố trong khoảng từ 1 đến 10 là: ");
+            for (int i = 2; i <= 10 ; i++)
+            {
+                if (CheckPrimeNumber(i))
+                {
+                    OnPrimeNumber = pnf;
+                    OnPrimeNumber(i);
+                }
+            }
+            
+            
+        }
+        
+        public static void NoticePrimeNumber(int pn)
+        {
+            Console.WriteLine("Prime Number: " + pn);
+        } 
+        
+        public static Boolean CheckPrimeNumber(int n)
+        {
+            int result = 0;
+            for (int i = 2; i <= n / 2; i++)
+            {
+                if (n % i == 0) result++;
+            }
+
+            if (result == 0) return true;
+            return false;
+        } 
+            
+        
+
     }
+    
+    
+    
+
 }
